60a223a75bd67b393110fe387a68a55a4d971efc
Anuradha-dol <anuradha123dk@gmail.com>
2026-04-21 03:06:30 +0530

    create services and some parts updated in some classes

---
+++ server/src/main/java/com/smartcampus/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.smartcampus.service;
+
+import com.smartcampus.dto.AuthResponse;
+import com.smartcampus.dto.UserDto;
+import com.smartcampus.records.LoginRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface AuthService {
+
+    AuthResponse signUp(UserDto.RegisterRequest registerRequest);
+
+    AuthResponse signIn(LoginRequest loginRequest, HttpServletResponse response);
+
+    AuthResponse verifyCode(String email, String verifyCode);
+
+    AuthResponse resendOtp(String email);
+}
